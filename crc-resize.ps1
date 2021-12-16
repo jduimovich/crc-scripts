1fa5ca99edcd556c43aa4bf8b87daee38bfb77f0
John Duimovich <jduimovich@gmail.com>
2021-12-16 15:34:46 -0500

    crc disk size config

---
+++ crc-resize.ps1
@@ -0,0 +1,5 @@
+ 
+ 
+crc stop
+Resize-VHD -Path %USERPROFILE%\.crc\machines\crc\crc.vhdx -SizeBytes 200GB
+crc start 
--- crcx.cmd
+++ crcx.cmd
@@ -1,15 +1,14 @@
-@echo off
-rem clean daemon 
-rem taskkill /IM crc.exe /F /FI "STATUS eq RUNNING"
-rem start /min crc.exe daemon
-rem timeout /t 2
-
-FOR /f "tokens=*" %%i IN ('crc oc-env') DO @call %%i
+@echo off 
 
 
-echo crc start -p %~dp0pull-secret.txt
-crc start   -p   %~dp0pull-secret.txt
- 
+echo crc start -p %~dp0pull-secret.txt --disk-size 100
+crc start   -p   %~dp0pull-secret.txt --disk-size 100
+
+FOR /f "tokens=*" %%i IN ('crc oc-env') DO @call %%i 
+
+rem CRC does no honour disk request so this manual first time
+rem in POWERSHELL FFS Resize-VHD -Path C:\Users\jduim\.crc\machines\crc\crc.vhdx -SizeBytes 200GB
+
 crc console --credentials  -o json  | jq .clusterConfig.adminCredentials.password >crc.info
 set /P LOGINQ=< crc.info
 del crc.info
@@ -20,8 +19,11 @@ set /P URLQ=< crc.info
 del crc.info
 Set URL=%URLQ:"=%
 
-oc login --insecure-skip-tls-verify=true -u kubeadmin -p %LOGIN% %URL%
-rem oc login --insecure-skip-tls-verify=true --token=%LOGIN% --server=%URL% 
+oc login --insecure-skip-tls-verify=true -u kubeadmin -p %LOGIN% %URL% 
 
 call crcpw.cmd		 
 crc console 
+
+crc status | grep Disk 
+echo "If memory is too small, resize the vm using powershell"
+echo "Resize-VHD -Path %HOMEDRIVE%%HOMEPATH%\.crc\machines\crc\crc.vhdx -SizeBytes 200GB"
