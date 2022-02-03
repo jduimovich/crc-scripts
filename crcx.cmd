74d5a1716b4756c01c85496b9581912873a968b8
John Duimovich <jduimovich@gmail.com>
2022-02-03 12:06:30 -0500

    refactor init

--- crcx.cmd
+++ crcx.cmd
@@ -1,26 +1,11 @@
 @echo off 
-
-
+rem start, login a kubeadmin, get the pw for console login
 echo crc start -p %~dp0pull-secret.txt --disk-size 100
 crc start   -p   %~dp0pull-secret.txt --disk-size 100
 
 FOR /f "tokens=*" %%i IN ('crc oc-env') DO @call %%i 
 
-rem CRC does no honour disk request so this manual first time
-rem in POWERSHELL FFS Resize-VHD -Path C:\Users\jduim\.crc\machines\crc\crc.vhdx -SizeBytes 200GB
-
-crc console --credentials  -o json  | jq .clusterConfig.adminCredentials.password >crc.info
-set /P LOGINQ=< crc.info
-del crc.info
-Set LOGIN=%LOGINQ:"=%
-
-crc console --credentials  -o json  | jq  .clusterConfig.url >crc.info
-set /P URLQ=< crc.info
-del crc.info
-Set URL=%URLQ:"=%
-
-oc login --insecure-skip-tls-verify=true -u kubeadmin -p %LOGIN% %URL% 
-
+call crck.cmd		
 call crcpw.cmd		 
 crc console 
 
